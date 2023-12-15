c0d09ed0a229e55d49f3b66c62710b763aecb779
Michael Lim <dongseok.lim@gmail.com>
2023-12-15 05:48:58 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./components/counter/Counter";
+import Todo from "./components/todo/Todo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
